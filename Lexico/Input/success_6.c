032f749f2e0b191067419ae122cd968d51e40c82
Thiago Veras <thiago.veras.machado@gmail.com>
2021-02-17 14:45:42 -0300

    2 Success Tests from commit

---
+++ Lexico/Input/success_6.c
@@ -0,0 +1,17 @@
+// Exemplo 1 Readme
+
+set subsum(set s, int target, int cur_sum, set ans) {
+    if(target == cur_sum) return ans;
+    else if (s == EMPTY) return EMPTY;
+    else {
+       int el;
+       remove((exists (el in s)) in s);
+       if(subsum(s, target, cur_sum, ans)) return ans;
+       cur_sum += el;
+       add(el in ans);
+       if(subsum(s, target, cur_sum, ans)) return ans;
+       add(el in s);
+       remove (el in s);
+       return EMPTY;
+    }
+}
--- Lexico/run.sh
+++ Lexico/run.sh
@@ -17,7 +17,7 @@ do
     i=$((i + 1))
 done
 
-SUCCESS_QNT=5
+SUCCESS_QNT=7
 i=1
 while [ $i -le $SUCCESS_QNT ]
 do
