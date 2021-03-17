53bfbfecbfdbe825c2a94d2e68a35d965dfa3313
VerasThiago <thiago.veras.machado@gmail.com>
2021-03-17 17:54:48 -0300

    test

--- Sintatico/Input/success_1.c
+++ Sintatico/Input/success_1.c
@@ -1,7 +1,9 @@
 int main(){
     if(f( (1 + 2) - b, 3 / (((3))), exists(b in add(1 in add(2 in s))))){
         write("puts");
-    } else if(13 in b){
+    }
+    if( (13 in b) || (is_set(b)) ){
         write("xota");
+        x = is_set(b);
     }
 }
