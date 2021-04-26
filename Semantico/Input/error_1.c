5a54bae6cda45a3d0e419c11d9744ed030f0ca54
VerasThiago <thiago.veras.machado@gmail.com>
2021-04-26 00:12:53 -0300

    Improve error file

--- Semantico/Input/error_1.c
+++ Semantico/Input/error_1.c
@@ -1,6 +1,6 @@
 set globalSet;
 
-int funcSuave(set s){
+int funcSuave(set s, int a, int b){
     return 0;
 }
 
@@ -8,6 +8,6 @@ int naoEhMain(){
     add(1 in globalSet);
     globalSet(10 + 3 / 2); // erro pois chama variavel como função
     if(2 + 3 / 2 > 0){
-        funcSuave(10); // erro pois não da pra converter int pra set;
+        funcSuave(10, globalSet, 10); // erro pois não da pra converter int pra set e nem set pra int (args 1 e 2);
     }
 }
